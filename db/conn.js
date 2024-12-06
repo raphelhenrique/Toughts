74d027912737113f9f4023fdc245f7863498f0bc
RaphaelHenriqueDev <167538225+raphelhenrique@users.noreply.github.com>
2024-12-06 13:29:09 -0300

    Add files via upload

---
+++ db/conn.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('toughts', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+})
+try {
+  sequelize.authenticate()
+  console.log('Conectado com sucesso')
+} catch (error) {
+  console.log(`Não foi possível conectar ${error}`)
+}
+module.exports = sequelize
